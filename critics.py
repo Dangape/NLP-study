95bea0233d8f0134c3e8736c8496236b947e7a85
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2020-12-31 14:08:14 -0300

    Update critics.py

--- critics.py
+++ critics.py
@@ -27,7 +27,7 @@ df = pd.DataFrame({"movies": [],
 data = []
 number = []
 
-teste = ["star_wars","borat","transformers_the_last_knight"]
+#teste = ["star_wars","borat","transformers_the_last_knight"]
 
 for k in range(0,len(titles)):
     movie = titles.iloc[k,0]
