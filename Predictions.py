8f38e0113920f31e8254496d6a76901df074ec57
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-01-07 02:26:29 -0300

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -16,20 +16,6 @@ critics = critics[['movies',"critics"]]
 critics = critics[critics['critics'].notnull()] #remove NaNs
 stop_words = stopwords.words("english")
 
-#Function to remove stop words from movies dataset
-def process_critic(critic):
-    processed_critic = critic
-    processed_critic = " ".join(word for word in processed_critic.split() if word not in stop_words) #remove stop words
-    return(processed_critic)
-
-
-critics["critics_processed"] = critics["critics"].apply(lambda x:process_critic(x)) #apply function to all DataFrame
-critics["critics_processed"] = critics["critics_processed"].apply(lambda x:re.findall(r"[\w']+|[.,!?;]",x)) #apply function to all DataFrame to split words and punctuation
-
-critics = critics["critics_processed"]
-critics = critics.values.tolist()
-print(critics[0])
-
 feature_vector = pickle.load(open("feature_vector.pickle", "rb"))
 
 # This is how the Naive Bayes classifier expects the input
@@ -38,6 +24,9 @@ def create_word_features(words):
     my_dict = dict([(word, True) for word in useful_words if word in useful_words])
     return my_dict
 
+#Loading saved model
+filename = 'finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
 test_data = "It would be impossible to sum up all the stuff that sucks about this film, so I'll break it down into what I remember most strongly: a man in an ingeniously fake-looking polar bear costume (funnier than the 'bear' " \
             "from Hercules in New York); an extra with the most unnatural laugh you're ever likely to hear; an ex-dope addict martian with tics; kid actors who make sure every syllable of their lines are slowly and caaarreee-fulll-yyy prrooo-noun-ceeed; " \
@@ -45,13 +34,17 @@ test_data = "It would be impossible to sum up all the stuff that sucks about thi
 
 words = word_tokenize(test_data)
 words = create_word_features(words)
-print(words)
-
-#Loading saved model
-filename = 'finalized_model.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
-
-print(loaded_model.labels())
 print(loaded_model.classify(words))
-pred = nltk.classify(loaded_model, words)
-print(pred)
+
+critics = pd.DataFrame(critics)
+critics["critics_processed"] = critics["critics"].apply(lambda x:word_tokenize(x))
+critics["critics_processed"] = critics["critics_processed"].apply(lambda x:create_word_features(x))
+critics["sentiment"] = critics["critics_processed"].apply(lambda x:loaded_model.classify(x))
+
+df = pd.DataFrame({"sentiment":[],
+                   "year":[]})
+
+neg = (len(critics[critics["sentiment"]=="neg"])/len(critics))*100
+pos = (len(critics[critics["sentiment"]=="pos"])/len(critics))*100
+
+print("{}% of the critics are negative".format(neg)+" and {}% are positive".format(pos))
